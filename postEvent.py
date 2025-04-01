dc0430eabd99dcefb3ae68e5b42633d26d52e072
helhindi <helhindi@outlook.com>
2025-04-01 19:09:02 +0100

    develop: Improved error handling scope via Augment.ai

--- postEvent.py
+++ postEvent.py
@@ -1,19 +1,16 @@
 import json
 import boto3
 from datetime import datetime
+from botocore.exceptions import ClientError
 
 def lambda_handler(event, context):
-
     dynamodb = boto3.resource('dynamodb')
-    client = boto3.client('dynamodb')
-    
     tableEvents = dynamodb.Table('events')
     
-    eventDateTime = (datetime.now()).strftime("%Y-%m-%d %H:%M:%S")
+    eventDateTime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     deviceId = event['deviceId']
 
     try:
-        
         tableEvents.put_item(
            Item={
                 'eventDateTime': eventDateTime,
@@ -23,11 +20,11 @@ def lambda_handler(event, context):
         
         return {
             'statusCode': 200,
-            'body': json.dumps('Succesfully submitted an event!')
+            'body': json.dumps('Successfully submitted an event!')
         }
-    except:
-        print('Closing lambda function')
+    except ClientError as e:
+        print(f'Error: {str(e)}')
         return {
                 'statusCode': 400,
-                'body': json.dumps('Error submitting the event')
+                'body': json.dumps(f'Error submitting the event: {str(e)}')
         }
